8d1d9444103eca9f97b35e961d6a6d2340fa3fab
Luka Teras <luka@sdf.org>
2024-12-10 17:22:20 +0000

    Rename `e` to `c`
    From u/inz__'s version.

--- nanoid.h
+++ nanoid.h
@@ -1,5 +1,5 @@
 #pragma once
 #include <stdlib.h>
 #include <unistd.h>
-char*nanoid(size_t t){char*b,*e=b=calloc(t+1,1);if(b){if(!getentropy(b,t))for(;t--;e++)*e=(*e&=63)<10?*e+48:*e<36?*e+87:*e<62?*e+29:*e>62?45:95;else free(b),b=0;}return b;}
+char*nanoid(size_t t){char*c,*b=c=calloc(t+1,1);if(b){if(!getentropy(b,t))for(;t--;c++)*c=(*c&=63)<10?*c+48:*c<36?*c+87:*c<62?*c+29:*c>62?45:95;else free(b),b=0;}return b;}
 #define NANOID()nanoid(21)
