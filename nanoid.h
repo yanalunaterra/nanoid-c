8ce9a2d3621fc4c7910387837bb8f202f92017cb
Luka Teras <luka@sdf.org>
2024-12-10 16:24:42 +0000

    Drop the whitespace between the macro name and the expansion
    Parens make this unambiguous.

--- nanoid.h
+++ nanoid.h
@@ -2,4 +2,4 @@
 #include <stdlib.h>
 #include <unistd.h>
 char*nanoid(size_t t){char*b,*e=b=calloc(t+1,1);if(b){if(!getentropy(b,t))while(t--){*e=(*e&=63)<10?*e+48:*e<36?*e+87:*e<62?*e+29:*e>62?45:95;e++;}else{free(b);b=0;}}return b;}
-#define NANOID() nanoid(21)
+#define NANOID()nanoid(21)
