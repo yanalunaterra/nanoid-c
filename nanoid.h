08cacf6a55df4a938192b9636010c1bf0e28fa15
Luka Teras <luka@sdf.org>
2024-12-10 17:23:32 +0000

    Rename `t` to `n`

--- nanoid.h
+++ nanoid.h
@@ -1,5 +1,5 @@
 #pragma once
 #include <stdlib.h>
 #include <unistd.h>
-char*nanoid(size_t t){char*c,*b=c=calloc(t+1,1);if(b){if(!getentropy(b,t))for(;t--;c++)*c=(*c&=63)<10?*c+48:*c<36?*c+87:*c<62?*c+29:*c>62?45:95;else free(b),b=0;}return b;}
+char*nanoid(size_t n){char*c,*b=c=calloc(n+1,1);if(b){if(!getentropy(b,n))for(;n--;c++)*c=(*c&=63)<10?*c+48:*c<36?*c+87:*c<62?*c+29:*c>62?45:95;else free(b),b=0;}return b;}
 #define NANOID()nanoid(21)
