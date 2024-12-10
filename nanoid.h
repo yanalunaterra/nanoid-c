9440f92cdeb7a73dd058c6a266b844d672e218f9
Luka Teras <luka@sdf.org>
2024-12-10 19:42:54 +0000

    Remove negation from the loop

--- nanoid.h
+++ nanoid.h
@@ -2,4 +2,4 @@
 #include<stdlib.h>
 #include<unistd.h>
 #define NANOID()nanoid(21)
-static char*nanoid(size_t n){char*c,*b=c=calloc(n+1,1);if(b){if(!getentropy(b,n))for(;n--;c++)*c=!(*c&=63)?45:*c<2?95:*c<12?*c+46:*c<38?*c+53:*c+59;else free(b),b=0;}return b;}
+static char*nanoid(size_t n){char*c,*b=c=calloc(n+1,1);if(b){if(!getentropy(b,n))for(;n--;c++)*c=(*c&=63)?*c<2?45:*c<12?*c+46:*c<38?*c+53:*c+59:95;else free(b),b=0;}return b;}
