eda8ef628f09c82277f756c89ac317aa44793245
Luka Teras <luka@sdf.org>
2024-12-10 20:31:16 +0000

    Rework the loop
    From u/gremolata's version. Thank you so much!

--- nanoid.h
+++ nanoid.h
@@ -2,4 +2,4 @@
 #include<stdlib.h>
 #include<unistd.h>
 #define NANOID()nanoid(21)
-static char*nanoid(size_t n){char*c,*b=c=calloc(n+1,1);if(b){if(!getentropy(b,n))for(;n--;c++)*c=(*c&=63)?*c<2?45:*c<12?*c+46:*c<38?*c+53:*c+59:95;else free(b),b=0;}return b;}
+static char*nanoid(size_t n){char*b=calloc(n+1,1),c;if(b){if(!getentropy(b,n))for(;n--;b[n]+=c?c<2?44:c<12?46:c<38?53:59:95)c=b[n]&=63;else free(b),b=0;}return b;}
