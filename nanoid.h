44b2ecc955106df7bf68266396ab1e9f20918a71
Luka Teras <luka@sdf.org>
2024-12-10 18:58:25 +0000

    Remove the whitespace from the includes

--- nanoid.h
+++ nanoid.h
@@ -1,5 +1,5 @@
 #pragma once
-#include <stdlib.h>
-#include <unistd.h>
+#include<stdlib.h>
+#include<unistd.h>
 static char*nanoid(size_t n){char*c,*b=c=calloc(n+1,1);if(b){if(!getentropy(b,n))for(;n--;c++)*c=!(*c&=63)?45:*c<2?95:*c<12?*c+46:*c<38?*c+53:*c+59;else free(b),b=0;}return b;}
 #define NANOID()nanoid(21)
