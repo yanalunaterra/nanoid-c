2e596caa34b4fe80015c8fbff154d0e119a30353
Luka Teras <luka@sdf.org>
2025-01-03 06:01:43 +0000

    `nanoidgen`: restructure to improve readability

--- nanoidgen.c
+++ nanoidgen.c
@@ -10,31 +10,40 @@
 
 int main(int argc, char *argv[]) {
   size_t length = NANOIDLEN;
-  if (argc == 2) {
-    length = strtoul(argv[1], NULL, 10);
-    if (errno) {
-      fprintf(stderr, "nanoidgen: Invalid length: %s\n", argv[1]);
-      return EXIT_FAILURE;
-    } else if (length < 1) {
-      fprintf(stderr, "nanoidgen: Minimum length 1, got: %zu\n", length);
-      return EXIT_FAILURE;
-    } else if (length > GETENTROPY_MAX) {
-      fprintf(stderr, "nanoidgen: Maximum length %u, got: %zu\n",
-              GETENTROPY_MAX, length);
-      return EXIT_FAILURE;
-    }
-  } else if (argc > 2) {
+
+  if (argc > 2) {
     fprintf(stderr, "nanoidgen: Expected at most 1 argument, got: %d\n",
             argc - 1);
     return EXIT_FAILURE;
   }
 
+  if (argc == 2) {
+    length = strtoul(argv[1], NULL, 10);
+
+    if (errno) {
+      fprintf(stderr, "nanoidgen: Invalid length: %s\n", argv[1]);
+      return EXIT_FAILURE;
+    }
+
+    if (length < 1) {
+      fprintf(stderr, "nanoidgen: Minimum length 1, got: %zu\n", length);
+      return EXIT_FAILURE;
+    }
+
+    if (length > GETENTROPY_MAX) {
+      fprintf(stderr, "nanoidgen: Maximum length %u, got: %zu\n",
+              GETENTROPY_MAX, length);
+      return EXIT_FAILURE;
+    }
+  }
+
   char *id = calloc(length + 1, sizeof(char));
-  if (!id || nanoidgen(id, length) || puts(id) < 0) {
+
+  if (!id || nanoidgen(id, length) || puts(id) == EOF) {
     perror("nanoidgen");
     return EXIT_FAILURE;
-  } else {
-    free(id);
-    return EXIT_SUCCESS;
   }
+
+  free(id);
+  return EXIT_SUCCESS;
 }
