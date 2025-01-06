05e2c2621264df0cefa282a9e035f9441fb5a67d
Manuel Bachmann <tarnyko@tarnyko.net>
2025-01-06 06:24:33 +0100

    Add Win32 support

--- nanoid.h
+++ nanoid.h
@@ -1,4 +1,12 @@
 #pragma once
-#include<unistd.h>
+#ifdef _WIN32
+#  define WIN32_LEAN_AND_MEAN
+#  include<windows.h>
+#  include<ntsecapi.h>
+#  define NANOIDGETENTROPY !RtlGenRandom
+#else
+#  include<unistd.h>
+#  define NANOIDGETENTROPY getentropy
+#endif
 #define NANOIDLEN 21
-static inline int nanoidgen(char*b,size_t n){int r=getentropy(b,n);if(!r)for(char c;n--;b[n]+=c?c<2?44:c<12?46:c<38?53:59:95)c=b[n]&=63;return r;}
+static inline int nanoidgen(char*b,size_t n){int r=NANOIDGETENTROPY(b,n);if(!r)for(char c;n--;b[n]+=c?c<2?44:c<12?46:c<38?53:59:95)c=b[n]&=63;return r;}
